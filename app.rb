1d3bac4640eec8b8ee647d3dc64a0abd06e4fd83
Nathanael Burt <nathanael.burt@gmail.com>
2014-04-06 12:39:31 -0600

    incorporated TasksHelper class into app

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
+require './lib/tasks_helper'
 
 class App < Sinatra::Application
-  TASK_LIST = []
+  TASK_LIST = TasksHelper.new
 
   get '/' do
-    erb :index, locals: {:task_list => TASK_LIST}
+    erb :index, locals: {:task_list => TASK_LIST.display_tasks}
   end
 
   get '/tasks/new' do
@@ -12,25 +13,25 @@ class App < Sinatra::Application
   end
 
   post '/' do
-    TASK_LIST << params[:add_task]
+    TASK_LIST.add(params[:add_task])
     redirect '/'
   end
 
   get '/tasks/:id' do
-    erb :show_task, locals: {:id => params[:id], :task => TASK_LIST[params[:id].to_i]}
+    erb :show_task, locals: {:id => params[:id], :task => TASK_LIST.display_tasks[params[:id].to_i]}
   end
 
   get '/tasks/:id/edit' do
-    erb :edit_task, locals: {:id => params[:id], :task => TASK_LIST[params[:id].to_i]}
+    erb :edit_task, locals: {:id => params[:id], :task => TASK_LIST.display_tasks[params[:id].to_i]}
   end
 
   put '/tasks/:id' do
-    TASK_LIST[params[:id].to_i] = params[:new_task_name]
+    TASK_LIST.edit(params[:id].to_i, params[:new_task_name].to_s)
     redirect '/'
   end
 
   delete '/tasks/:id' do
-    TASK_LIST.delete_at(params[:id].to_i)
+    TASK_LIST.delete(params[:id].to_i)
     redirect '/'
   end
 end
